cdf4989ece4c85f73ded04502a3af43b23bf444a
Antonio Raimundo <antoniorrm@Mac-mini-Dj.local>
2020-11-20 17:25:38 -0300

    update class for send msg

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const { app, BrowserWindow } = require("electron");
 const express = require("express");
 
-app.on("ready", function() {
+app.on("ready", function () {
   var ex = express();
 
   var mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
-  ex.get("/whats/:num/:msg", function(req, res) {
+  ex.get("/whats/:num/:msg", function (req, res) {
     var numero = req.params.num;
     var msg = req.params.msg;
     enviar(numero, msg);
@@ -24,11 +24,11 @@ app.on("ready", function() {
       "https://web.whatsapp.com/send?phone=" + telefone + "&text=" + mensagem,
       {
         userAgent:
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36"
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36",
       }
     );
     mainWindow.webContents.executeJavaScript(
-      'var{ipcRenderer,remote} = require("electron");var enviado = false;function tempo(){var btsend = document.getElementsByClassName("_3M-N-")[0];var inputSend = document.getElementsByClassName("_3u328")[1];if(typeof inputSend !== "undefined" && inputSend.innerText && !enviado){btsend.click();enviado=true;}else if(enviado){ipcRenderer.send("para", {status:true});}}setInterval(tempo,3000);'
+      'var{ipcRenderer,remote} = require("electron");var enviado = false;function tempo(){var btsend = document.getElementsByClassName("_2Ujuu")[0];var inputSend = document.getElementsByClassName("_1awRl")[1];if(typeof inputSend !== "undefined" && inputSend.innerText && !enviado){btsend.click();enviado=true;}else if(enviado){ipcRenderer.send("para", {status:true});}}setInterval(tempo,3000);'
     );
     mainWindow.show();
   }
